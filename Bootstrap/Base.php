f1afb7942bdd9d5b7cb72ea78ef9d2e342fb56d3
tanbiao <tanb@51kingstone.com>
2017-09-06 18:26:15 +0800

    boot

--- Bootstrap/Base.php
+++ Bootstrap/Base.php
@@ -7,6 +7,6 @@ require_once dirname(__DIR__) . '/Utils/helpers.php';
 define('APP_ROOT', dirname(__DIR__));
 
 try {
-    (new Dotenv\Dotenv(APP_ROOT))->load();
+//    (new Dotenv\Dotenv(APP_ROOT))->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
 }
