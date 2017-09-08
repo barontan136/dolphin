0ccd5b1fc3a77d0632d73ec9f450dda5d8301614
tanbiao <tanb@51kingstone.com>
2017-09-08 19:18:54 +0800

    websocket

--- Tasks/Start_Websocket.php
+++ Tasks/Start_Websocket.php
@@ -7,7 +7,7 @@ defined('MODULE_NAME') || define('MODULE_NAME', 'WebSocket');
 use Workerman\Worker;
 
 // 加载所有Applications/*/start.php，以便启动所有服务
-foreach(glob(__DIR__.'/websocket/start*.php') as $start_file)
+foreach(glob(__DIR__.'/../websocket/start*.php') as $start_file)
 {
     require_once $start_file;
 }
