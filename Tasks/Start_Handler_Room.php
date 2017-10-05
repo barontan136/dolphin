0e903cad38925231c9031b97b03f157901f4a3fd
baron <40428016@qq.com>
2017-10-05 18:34:51 +0800

    add updateTitle interface

--- Tasks/Start_Handler_Room.php
+++ Tasks/Start_Handler_Room.php
@@ -6,9 +6,9 @@ defined('MODULE_NAME') || define('MODULE_NAME', 'Room');
 
 use Workerman\Worker;
 use Utils\Task;
-use Handlers\UserHandler;
+use Handlers\RoomHandler;
 
-$task = new Task('Room', UserHandler::class);
+$task = new Task('Room', RoomHandler::class);
 // task进程数可以根据需要多开一些
 
 if (!defined('GLOBAL_START')) {
