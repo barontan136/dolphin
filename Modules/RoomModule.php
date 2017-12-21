375f4b65309bb29316e0ad504d8bb6c9eceed074
tanbiao <tanb@51kingstone.com>
2017-12-21 14:08:45 +0800

    debug

--- Modules/RoomModule.php
+++ Modules/RoomModule.php
@@ -57,66 +57,71 @@ class RoomModule
      */
     public function getRoomDetail($user_id, $rid)
     {
-        // 获取房间信息
-        $roomTable = new RoomTable();
-        $roomInfo = $roomTable->getRoomInfo(['rid' => $rid]);
-        if (empty($roomInfo)) return false;
+        try {
 
-        // 获取礼物列表
-        $giftTable = new GiftTable();
-        $giftList = $giftTable->getGiftList();
-        // 获取用户关注关系
-        $userAttention = new UserAttentionTable();
-        $love = $userAttention->checkAttUsers($user_id, $roomInfo['uid']);
-        // 获取主播管理员列表ID
-        $roomAdminTable = new RoomAdminTable();
-        $adminUids = $roomAdminTable->getRoomAdminIdsByID($roomInfo['rid']);
-        // 获取主播详细信息
-        $userInfo = $this->userTable->getUserInfoByUserId($roomInfo['uid']);
-        $moderator = array(
-            'weight'            => 0,
-            'height'            => 0,
-            'age'               => 0,
-            'trueName'          => $userInfo['realName'],
-            'nickname'          => $userInfo['nickname'],
-            'id'                => $userInfo['mid'],
-            'moderatorLevel'    => $userInfo['moderatorLevel'],
-            'nextLevelNeed'     => $userInfo['moderatorNextLevelNeedCoin'],
-            'levelEarnCoin'     => $userInfo['moderatorLevelCoin'],
-            'headPic'           => $userInfo['headPic'],
-            'verified'          => isset($userInfo['verified']) ? $userInfo['verified'] : '',
-            'verifyInfo'        => isset($userInfo['verifyInfo']) ? $userInfo['verifyInfo'] : '',
-            'earnCoin'          => 0,
-        );
+            // 获取房间信息
+            $roomTable = new RoomTable();
+            $roomInfo = $roomTable->getRoomInfo(['rid' => $rid]);
+            if (empty($roomInfo)) return false;
 
-        $result = array(
-            'rid'               => $roomInfo['rid'],
-            'autoID'            => $roomInfo['autoID'],
-            'msgIP'             => $roomInfo['msgIP'],
-            'msgPort'           => $roomInfo['msgPort'],
-            'videoPlayDomain'   => $roomInfo['videoPlayDomain'],
-            'videoPublishDomain'=> $roomInfo['videoPublishDomain'],
-            'videoPath'         => $roomInfo['videoPath'],
-            'videoStreamName'   => Common::get_play_url(Rtmp::$tx_biz_id, $roomInfo['rid']),
-            'videoPlayUrl'      => Common::get_play_url(Rtmp::$tx_biz_id, $roomInfo['rid']),
-            'flowerNumber'      => $roomInfo['flowerNumber'],
-            'isPlaying'         => $roomInfo['isPlaying'],
-            'onlineNum'         => $roomInfo['onlineNum'],
-            'danmuBg'           => $roomInfo['danmuBg'],
-            'shareTitle'        => $roomInfo['shareTitle'],
-            'shareContent'      => $roomInfo['shareContent'],
-            'sharePic'          => $roomInfo['sharePic'],
-            'shareUrl'          => $roomInfo['shareUrl'],
-            'private'           => $roomInfo['private'],
-            'messages'          => '',
-            'userType'          => $userInfo['type'],
-            //
-            'isGuard'           => 0,
-            'loved'              => $love ? 'true' : 'false',
-            'adminUids'         => $adminUids,
-            'moderator'         => $moderator,
-            'gifts'             => $giftList,
-        );
+            // 获取礼物列表
+            $giftTable = new GiftTable();
+            $giftList = $giftTable->getGiftList();
+            // 获取用户关注关系
+            $userAttention = new UserAttentionTable();
+            $love = $userAttention->checkAttUsers($user_id, $roomInfo['uid']);
+            // 获取主播管理员列表ID
+            $roomAdminTable = new RoomAdminTable();
+            $adminUids = $roomAdminTable->getRoomAdminIdsByID($roomInfo['rid']);
+            // 获取主播详细信息
+            $userInfo = $this->userTable->getUserInfoByUserId($roomInfo['uid']);
+            $moderator = array(
+                'weight' => 0,
+                'height' => 0,
+                'age' => 0,
+                'trueName' => $userInfo['realName'],
+                'nickname' => $userInfo['nickname'],
+                'id' => $userInfo['mid'],
+                'moderatorLevel' => $userInfo['moderatorLevel'],
+                'nextLevelNeed' => $userInfo['moderatorNextLevelNeedCoin'],
+                'levelEarnCoin' => $userInfo['moderatorLevelCoin'],
+                'headPic' => $userInfo['headPic'],
+                'verified' => isset($userInfo['verified']) ? $userInfo['verified'] : '',
+                'verifyInfo' => isset($userInfo['verifyInfo']) ? $userInfo['verifyInfo'] : '',
+                'earnCoin' => 0,
+            );
+
+            $result = array(
+                'rid' => $roomInfo['rid'],
+                'autoID' => $roomInfo['autoID'],
+                'msgIP' => $roomInfo['msgIP'],
+                'msgPort' => $roomInfo['msgPort'],
+                'videoPlayDomain' => $roomInfo['videoPlayDomain'],
+                'videoPublishDomain' => $roomInfo['videoPublishDomain'],
+                'videoPath' => $roomInfo['videoPath'],
+                'videoStreamName' => Common::get_play_url(Rtmp::$tx_biz_id, $roomInfo['rid']),
+                'videoPlayUrl' => Common::get_play_url(Rtmp::$tx_biz_id, $roomInfo['rid']),
+                'flowerNumber' => $roomInfo['flowerNumber'],
+                'isPlaying' => $roomInfo['isPlaying'],
+                'onlineNum' => $roomInfo['onlineNum'],
+                'danmuBg' => $roomInfo['danmuBg'],
+                'shareTitle' => $roomInfo['shareTitle'],
+                'shareContent' => $roomInfo['shareContent'],
+                'sharePic' => $roomInfo['sharePic'],
+                'shareUrl' => $roomInfo['shareUrl'],
+                'private' => $roomInfo['private'],
+                'messages' => '',
+                'userType' => $userInfo['type'],
+                //
+                'isGuard' => 0,
+                'loved' => $love ? 'true' : 'false',
+                'adminUids' => $adminUids,
+                'moderator' => $moderator,
+                'gifts' => $giftList,
+            );
+        }catch (\Exception $e){
+            $this->log->error(sprintf("getRoomDetail:%s", $e->getMessage()));
+        }
         return $result;
     }
 
